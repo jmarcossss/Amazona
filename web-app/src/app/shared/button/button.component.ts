cfae4bc5643e8aaadc57b9ae65687b667548893f
mariavmuniz <soaresmunizm@gmail.com>
2023-03-23 18:49:39 -0300

    style(shared): add parameter in button style

--- web-app/src/app/shared/button/button.component.ts
+++ web-app/src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+  @Input() type: string = 'btn large'
+  @Input() label: string = 'Button';
+
+   onClick() {
+    console.log('Button clicked!');
+  }
 
 }
