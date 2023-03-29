7ab5d04891bb64f37e9628f14a45bfa83b37ab74
Andresa <aas10@cin.ufpe.br>
2023-03-29 00:48:10 -0300

    feat(user): add user.entity and user.model

---
+++ server/src/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import BaseEntity from './base.entity';
+
+export default class UserEntity extends BaseEntity {
+    CPF: string;
+    name: string;
+    username: string;
+    email: string;
+    password: string; 
+    payment: string;
+    address: string[];
+    phone: string;
+
+    constructor(data: Partial<UserEntity>){
+        super(data.id || '');
+        this.CPF = data.CPF || '';
+        this.name = data.name || '';
+        this.username = data.username || '';
+        this.email = data.email || '';
+        this.password = data.password || '';
+        this.payment = data.payment || '';
+        this.address = data.address || [];
+        this.phone = data.phone || '';
+    }
+}
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,11 +1,46 @@
+import OrderEntity from '../entities/order.entity';
 import BaseModel from './base.model';
 
 class UserModel extends BaseModel {
+  CPF: string;
   name: string;
+  username: string;
+  email: string;
+  password: string; 
+  payment: string;
+  address: string[];
+  phone: string;
 
-  constructor({ id, name }: { id: string; name: string }) {
+  constructor({ 
+    id,
+    CPF, 
+    name, 
+    username,
+    email, 
+    password, 
+    payment, 
+    address, 
+    phone,
+  } : { 
+    id: string,
+    CPF: string; 
+    name: string; 
+    username: string; 
+    email: string;  
+    password: string;
+    payment: string;
+    address: string[];
+    phone: string;
+  }) {
     super(id);
+    this.CPF = CPF;
     this.name = name;
+    this.username = username;
+    this.email =email;  
+    this.password = password;
+    this.payment = payment;
+    this.address = address;
+    this.phone = phone;
   }
 }
 
