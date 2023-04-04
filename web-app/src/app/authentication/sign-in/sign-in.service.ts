4da4ece0ce9ce3fcecc241889c0d5f6e2c8f440e
mariavmuniz <soaresmunizm@gmail.com>
2023-04-03 23:19:25 -0300

    feat(login): add connection with server

--- web-app/src/app/authentication/sign-in/sign-in.service.ts
+++ web-app/src/app/authentication/sign-in/sign-in.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BaseService } from '../../services/base.service';
+import { HttpService, Response } from '../../services/http.service';
+import { map } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class SignInService {
+export class SignInService extends BaseService{
   public signInForm: FormGroup;
+  private prefix: string = '/authentication/sign-in';
+  signInService: any;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private httpService: HttpService,  signInService: SignInService) {
+    super()
     this.signInForm = this.formBuilder.group({
       user: ['', Validators.required],
       password: [
@@ -23,11 +30,22 @@ export class SignInService {
     });
   }
 
+  signIn(data: any) {
+    let jsonData = JSON.stringify(data);
+
+    return this.httpService.post(this.prefix, jsonData).pipe(
+      map((response: Response) => response),
+    );
+  }
   public submitForm(): void {
     this.signInForm.markAllAsTouched();
 
-    if (this.signInForm.valid) {
-      // TODO: Request login
+     if (this.signInForm.valid) {
+      this.signInService(this.signInForm.getRawValue()).subscribe((response: Response) => {
+        console.warn(response);
+      });
     }
   }
 }
+
+
