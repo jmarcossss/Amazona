28bfee695450ee5333439bc885c44508529b5c78
jmarcossss <joaomalcantaraaa@gmail.com>
2023-04-05 00:28:45 -0300

    feat(*): initializing follow screen

--- web-app/src/app/order/follow/follow.component.ts
+++ web-app/src/app/order/follow/follow.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-follow',
   templateUrl: './follow.component.html',
-  styleUrls: ['./follow.component.css']
+  styleUrls: ['./follow.component.css'],
 })
 export class FollowComponent {
 
