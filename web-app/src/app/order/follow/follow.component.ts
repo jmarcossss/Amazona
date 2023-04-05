721c6f1817fec774ad026c5b7e2892f0f3aae329
jmarcossss <joaomalcantaraaa@gmail.com>
2023-04-05 13:01:45 -0300

    feat(follow): adding orderId

--- web-app/src/app/order/follow/follow.component.ts
+++ web-app/src/app/order/follow/follow.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-follow',
   templateUrl: './follow.component.html',
   styleUrls: ['./follow.component.css'],
 })
-export class FollowComponent {
+export class FollowComponent implements OnInit {
+  orderId!: number; 
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.orderId = this.route.snapshot.params['id']; // colchetes para acessar a propriedade do id
+  }
 }
