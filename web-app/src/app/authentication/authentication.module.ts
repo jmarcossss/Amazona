0f7c3041076757e5538b31c06aaf0c1140d87ba3
Victor Silva <victormarques.ia@gmail.com>
2023-04-09 21:22:33 -0300

    feat(authentication): save user data on local storage

--- web-app/src/app/authentication/authentication.module.ts
+++ web-app/src/app/authentication/authentication.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { SignInModule } from './sign-in/sign-in.module';
 import { RecoverPasswordModule } from './recover-password/recover-password.module';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { AuthenticationRoutingModule } from './authentication-routing.module';
-import { SharedModule } from '../shared/shared.module';
 import { SignUpModule } from './sign-up/sign-up.module';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [],
   imports: [
     CommonModule,
     AuthenticationRoutingModule,
--- web-app/src/app/models/user.model.ts
+++ web-app/src/app/models/user.model.ts
@@ -1,8 +1,42 @@
-export class UserModel {
-  id: string = '';
-  name: string = '';
+import BaseModel from './base.model';
 
-  constructor(data?: Partial<UserModel>) {
-    Object.assign(this, data);
+class UserModel extends BaseModel {
+  CPF: string;
+  name: string;
+  username: string;
+  email: string;
+  payment: string;
+  address: string[];
+  phone: string;
+
+  constructor({
+    id,
+    CPF,
+    name,
+    username,
+    email,
+    payment,
+    address,
+    phone,
+  }: {
+    id: string;
+    CPF: string;
+    name: string;
+    username: string;
+    email: string;
+    payment: string;
+    address: string[];
+    phone: string;
+  }) {
+    super(id);
+    this.CPF = CPF;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.payment = payment;
+    this.address = address;
+    this.phone = phone;
   }
 }
+
+export default UserModel;
