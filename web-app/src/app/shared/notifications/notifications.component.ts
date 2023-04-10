00b8310404e741c14141b3b8508b53be2417c5a8
RafaelRL3 <rafellabio2@gmail.com>
2023-04-09 21:02:40 -0300

    wip(notifications): Passing selected data to back-end

--- web-app/src/app/shared/notifications/notifications.component.ts
+++ web-app/src/app/shared/notifications/notifications.component.ts
@@ -1,16 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiMessageCodes } from '../../shared/utils/api-message-codes';
+import { Router } from '@angular/router';
+import { SnackBarService } from '../../services/snack-bar.service';
+import { FormGroup } from '@angular/forms';
+import { NotificationsService } from './notifications.services';
+import { FormControl } from '@angular/forms';
+import NotificationModel from 'src/app/models/notification.model';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent {
 
+export class NotificationsComponent implements OnInit {
+  notificationForm!: FormGroup;
+  notifications: NotificationModel[];
   Data: Date = new Date();
 
-  dataSelecionada(selectedDate: Date){
+  constructor(
+    private notificationService: NotificationsService,
+    private snackBarService: SnackBarService,
+    private router: Router,
+  ) {
+    this.notifications = []
+  }
+
+  ngOnInit() {
+    this.clean()
+    this.notificationForm = this.notificationService.notificationForm
+
+    this.notificationService.notificationStatus$.subscribe((status) => {
+      status.maybeMap({
+        succeeded: (notifications: any[]) => {
+          console.warn(notifications);
+          this.notifications = notifications;
+        },
+        failed: (_) => {
+          this.snackBarService.showError({
+            message: 'error loading notifications',
+          });
+        },
+      });
+    });
+  }
+  
+  public async clean(): Promise<void> {
+    this.notificationService.clean()
+    await this.buscar();
+  }
+  setAtivo(): void {
+    this.notificationService.setAtivo();
+  }
+  setConcluido(): void {
+    this.notificationService.setConcluido();
+  } setCancelado(): void {
+    this.notificationService.setCancelado();
+  }
+  
+  public async buscar(selectedDate?: Date): Promise<void> {
+    this.notifications = await this.notificationService.buscar(selectedDate);    
+    console.log(this.notifications)
+  }
+
+
+  dataSelecionada(selectedDate: Date) {
     this.Data = selectedDate;
+    this.buscar(selectedDate);
   }
 
 }
---
+++ web-app/src/app/shared/notifications/notifications.services.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BaseService } from '../../services/base.service';
+import { HttpService, Response } from '../../services/http.service';
+import { BehaviorSubject, firstValueFrom, map } from 'rxjs';
+import { RequestStatus } from '../utils/request-status';
+import { ErrorResponse } from '../utils/response';
+import NotificationModel from 'src/app/models/notification.model';
+import { NotificationsComponent } from './notifications.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class NotificationsService extends BaseService {
+  private prefix: string = '/notifications/user';
+  private userId: string = 'ce6f5c66-1967-4b21-9929-51ca7d652151'
+  public notificationForm: FormGroup;
+  statusAtivo: boolean;
+  statusProcessando: boolean;
+  statusCancelado: boolean;
+  actualDate;
+
+  public notificationStatus: BehaviorSubject<RequestStatus<any, ErrorResponse>> =
+    new BehaviorSubject<RequestStatus<any, ErrorResponse>>(
+      RequestStatus.idle()
+    );
+
+  public notificationStatus$ = this.notificationStatus.asObservable();
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService
+  ) {
+    super()
+    this.notificationForm = this.formBuilder.group({
+      dateActual: ''
+    });
+    this.actualDate = this.notificationForm.get('dateActual');
+    alert(this.actualDate?.value)
+    this.statusAtivo = false;
+    this.statusProcessando = false;
+    this.statusCancelado = false;
+  }
+
+  public setAtivo(): void {
+    this.statusAtivo = true;
+  }
+
+  public setConcluido(): void {
+    this.statusProcessando = true;
+  }
+
+  public setCancelado(): void {
+    this.statusCancelado = true;
+  }
+
+  public clean(): void {
+    this.statusAtivo = false;
+    this.statusProcessando = false;
+    this.statusCancelado = false;
+    this.actualDate?.setValue("")
+  }
+
+  public async buscar(selectedDate?: Date ): Promise<NotificationModel[]> {
+    console.log(`${selectedDate}`)
+    let uri: string = `${this.prefix}/${this.userId}?`
+    if (!!selectedDate) {
+      console.log(`entrando em datas`)
+      uri += `date=${encodeURIComponent(selectedDate?.toISOString())}`
+    }
+
+    const response = await firstValueFrom(
+      this.httpService.get(uri)
+    );
+    let resposta: NotificationModel[] = []
+    response.handle({
+      onSuccess: (resp) => {
+        const notifications = (resp.data as any[]).map((notification: any) => new NotificationModel(notification))
+        this.notificationStatus.next(RequestStatus.success(resp));
+        resposta = notifications
+      },
+      onFailure: (error) => {
+        this.notificationStatus.next(RequestStatus.failure(error));
+      },
+    });
+    return (resposta as NotificationModel[])
+  }
+
+
+
+}
